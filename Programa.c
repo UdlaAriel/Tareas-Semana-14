cf54db038b3747a6dbda8867b9d444b350b9df4a
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-06-26 16:37:12 -0500

    Add files via upload

--- Programa.c
+++ Programa.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
+#define MAX 20
+
 int main (void)
 {
+    struct datosPersonales
+    {
+        char nombre[MAX];
+        char direccion[MAX];
+        char carrera[MAX];
+        float promedio;
+    } datos;
+    
+    printf("Ingrese su nombre: ");
+    gets(datos.nombre);
+
+    printf("Ingrese su direccion: ");
+    gets(datos.direccion);
+
+    printf("Ingrese su carrera: ");
+    gets(datos.carrera);
+
+    printf("Ingrese su promedio: ");
+    scanf("%f",&datos.promedio);
+
     FILE *archivo;
     char caracter;
     
-    
-    archivo = fopen("prueba.txt","w"); /*Definimos el nombre del archivo que utilizaremos, además de colocar qué haremos con el archivo (Revisar cada nodo)*/
-    
-    fputs("Esta es una prueba de escritura",archivo);
+    archivo = fopen("estructura2.txt","w"); /*Definimos el nombre del archivo que utilizaremos, además de colocar qué haremos con el archivo (Revisar cada nodo)*/
+    fputs(datos.nombre,archivo);
+    fputs(datos.direccion,archivo);
+    fputs(datos.carrera,archivo);
 
-    /*
-    while (!feof(archivo))
-    {
-        caracter = fgetc(archivo);
-        putchar(caracter);
-    }
-    */
+    fprintf(archivo, "%.2f", datos.promedio);
+    // fputs(datos.promedio,archivo);
 
     fclose(archivo);
 
