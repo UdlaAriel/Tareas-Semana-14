84c9931e146f69be9da9a749dfb5626e3c221881
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-06-26 16:00:14 -0500

    Se añade la impresión en pantalla del contenido
    Se imprime en la pantalla el contenido que se encuentra en el archivo .txt

--- Programa.c
+++ Programa.c
@@ -3,12 +3,14 @@
 int main (void)
 {
     FILE *archivo;
+    char caracter;
 
     archivo = fopen("hola.txt","r"); /*Definimos el nombre del archivo que utilizaremos, además de colocar qué haremos con el archivo (Revisar cada mod)*/
 
     while (!feof(archivo))
     {
-        fgetc(archivo);
+        caracter = fgetc(archivo);
+        putchar(caracter);
     }
     
     fclose(archivo);
