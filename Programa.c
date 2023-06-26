d97a1782b762dd404bfb838726231bf599dbadf8
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-06-26 15:48:35 -0500

    Add files via upload

---
+++ Programa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    FILE *archivo;
+
+    archivo = fopen("hola.txt","r"); /*Definimos el nombre del archivo que utilizaremos, además de colocar qué haremos con el archivo (Revisar cada mod)*/
+
+    return 0;
+}
